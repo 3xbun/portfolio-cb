86f47a52085fc715639b4c35b7e13de30ce1b1af
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-06-06 23:54:59 +0700

    Updates

---
+++ precache-manifest.9c24ab1e852c8b1b96a8c5b35e356cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec4979d07fd06adcf1e4a33c4bd916b",
+    "url": "/portfolio-cb/index.html"
+  },
+  {
+    "revision": "1df83d9369cede60d1c5",
+    "url": "/portfolio-cb/static/css/main.78eac6f8.chunk.css"
+  },
+  {
+    "revision": "e6ffe55e108fc95fb12d",
+    "url": "/portfolio-cb/static/js/2.198af6bd.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/portfolio-cb/static/js/2.198af6bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df83d9369cede60d1c5",
+    "url": "/portfolio-cb/static/js/main.0413b25a.chunk.js"
+  },
+  {
+    "revision": "39057ff33d0017e3142d",
+    "url": "/portfolio-cb/static/js/runtime-main.49532966.js"
+  },
+  {
+    "revision": "626eb36fd4f7e736bc59b97a42b444ee",
+    "url": "/portfolio-cb/static/media/profileImg.626eb36f.jpg"
+  }
+]);
